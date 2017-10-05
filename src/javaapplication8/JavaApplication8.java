458b69b689ea0e1aa4bb8f91085e80819e141f3d
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2017-10-05 10:01:42 +0200

    Inicio proyecto

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -4,7 +4,7 @@ package javaapplication8;
 public class JavaApplication8 {
 
     public static void main(String[] args) {
-        //Mensaje en pantalla
+        
         System.out.println("Hola mundo!");     
     }
 }
